91a4eef5f88df012b84cfab8b67a8f99e0bbf7d1
aelaf-git <aelafeskindir124@gmail.com>
2026-02-28 15:38:34 +0300

    Enhance index view to include latest items and categories in context

--- market_place/core/views.py
+++ market_place/core/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from .models import Category, Item
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False).order_by('-created_at')[:10]
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'items': items,
+        'categories': categories,
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
