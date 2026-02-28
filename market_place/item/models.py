37cb9ec5dbb41f6598bd994c822c792427dd0718
aelaf-git <aelafeskindir124@gmail.com>
2026-02-28 15:31:23 +0300

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- market_place/item/models.py
+++ market_place/item/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    class Meta:
+        verbose_name_plural = 'Categories'
+    def __str__(self):
+        return self.name
 
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('auth.User', related_name='items', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='item_images/', blank=True, null=True)
     def __str__(self):
         return self.name
