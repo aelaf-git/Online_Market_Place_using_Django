27f8c4ac5d7bb346eb961c1f55dfdb4a17d8dabb
aelaf-git <aelafeskindir124@gmail.com>
2026-03-04 21:25:15 +0300

    Implement signup form with email and password fields, and handle form submission in views

--- market_place/core/forms.py
+++ market_place/core/forms.py
@@ -13,3 +13,15 @@ class SignupForm(UserCreationForm):
         'placeholder': 'Username',
         'class': 'w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500',
     }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Email',
+        'class': 'w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500',
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'password',
+        'class': 'w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500',
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'confirm password',
+        'class': 'w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500',
+    }))
