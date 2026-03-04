a724cceb0407185f50dcfa0a5b2cb4121a8ddb47
aelaf-git <aelafeskindir124@gmail.com>
2026-03-04 21:16:35 +0300

    Add username field to signup form with custom styling

--- market_place/core/forms.py
+++ market_place/core/forms.py
@@ -8,3 +8,8 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+    
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Username',
+        'class': 'w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500',
+    }))
