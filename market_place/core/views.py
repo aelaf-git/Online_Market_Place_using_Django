0b35426f2912569736cdbb78857b966026f9ba44
aelaf-git <aelafeskindir124@gmail.com>
2026-03-04 20:09:34 +0300

    Add signup form implementation to core views

--- market_place/core/views.py
+++ market_place/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # core/views.py
 from item.models import Category, Item
-
+from .forms import SignupForm
 # Create your views here.
 
 def index(request):
@@ -15,3 +15,10 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form,
+    })
