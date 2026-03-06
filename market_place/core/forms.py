7d5df6cc00f1fcf73a2d00f29d9b8e04735967ec
aelaf-git <aelafeskindir124@gmail.com>
2026-03-06 22:48:20 +0300

    Add login functionality with custom login form and template

--- market_place/core/forms.py
+++ market_place/core/forms.py
@@ -1,6 +1,16 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Username',
+        'class': 'w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500',
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'password',
+        'class': 'w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500',
+    }))
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(required=True)
