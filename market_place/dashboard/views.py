b4f3e4ed20074e36447b7050657a60044c706481
aelaf-git <aelafeskindir124@gmail.com>
2026-03-18 17:04:56 +0300

    Delete item Added

--- market_place/dashboard/views.py
+++ market_place/dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from item.models import Item
@@ -7,3 +7,4 @@ from item.models import Item
 def index(request):
     items = Item.objects.filter(created_by=request.user)
     return render(request, 'dashboard/index.html', {'items': items})
+
