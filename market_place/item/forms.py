73921c823b4e927993c28bde9bae08e78cc3152b
aelaf-git <aelafeskindir124@gmail.com>
2026-03-18 17:30:53 +0300

    Edit Items is Updated

--- market_place/item/forms.py
+++ market_place/item/forms.py
@@ -28,4 +28,26 @@ class NewItemForm(forms.ModelForm):
                 'class': INPUT_CLASSES
             })
         }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'is_sold')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Item Name',
+                'class': INPUT_CLASSES,
+           }),
+            'description': forms.Textarea(attrs={
+                'placeholder': 'Item Description',
+                'class': INPUT_CLASSES,
+           }),
+            'price': forms.NumberInput(attrs={
+                'placeholder': 'Price in USD',
+                'class': INPUT_CLASSES,
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            })
+        }
         
