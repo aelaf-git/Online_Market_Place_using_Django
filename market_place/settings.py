e2ceb5e22f4ba9b5a4c0dbf5b87a6249d9ef0f64
aelaf-git <aelafeskindir124@gmail.com>
2026-04-01 16:41:01 +0300

    refactor: configure database using dj_database_url and remove local development files

--- market_place/settings.py
+++ market_place/settings.py
@@ -90,10 +90,9 @@ WSGI_APPLICATION = 'market_place.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=env('DATABASE_URL')
+    )
 }
 
 
