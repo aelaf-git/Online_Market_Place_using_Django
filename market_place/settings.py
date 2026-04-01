d5ae9efad50de177e5de236c1d87a660ad564371
aelaf-git <aelafeskindir124@gmail.com>
2026-04-01 16:24:28 +0300

    refactor: switch database configuration to local SQLite and add data dump file

--- market_place/settings.py
+++ market_place/settings.py
@@ -90,9 +90,10 @@ WSGI_APPLICATION = 'market_place.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=env('DATABASE_URL', default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
