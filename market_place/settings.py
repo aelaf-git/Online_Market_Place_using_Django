f6fce3cbfd925e6ad0a4379641fb33f3e61ba318
aelaf-git <aelafeskindir124@gmail.com>
2026-04-04 14:37:57 +0300

    fix: set STATIC_ROOT and enable WhiteNoise for production

--- market_place/settings.py
+++ market_place/settings.py
@@ -16,12 +16,12 @@ env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gvxdc8%l-)ecplb)b!1fwp84_+g(y)!z&v=t0195+b195x+25-'
+SECRET_KEY = env('SECRET_KEY', default='django-insecure-gvxdc8%l-)ecplb)b!1fwp84_+g(y)!z&v=t0195+b195x+25-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# WhiteNoise storage for compressed/hashed static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
